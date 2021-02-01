8ad110cc249d92e100682e9c9732b06f0f63cc9b
James <57846197+jrilett@users.noreply.github.com>
2021-02-01 19:31:17 +0000

    Update FilmsStatistics.py

--- FilmsStatistics.py
+++ FilmsStatistics.py
@@ -11,6 +11,10 @@ import base64
 from oauth2client.service_account import ServiceAccountCredentials
 from pandas.io.json import json_normalize
 
+# Creates a cache and allows data to be stored to ensure faster running
+@st.cache(persist=True)
+@st.cache(suppress_st_warning=True)
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Layout functions
